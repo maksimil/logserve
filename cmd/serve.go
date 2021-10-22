5a84d836692c7e85571da9eb9e1a53fbcbcc456f
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-10-22 19:39:28 +0300

    added data/json endpoint

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/spf13/cobra"
@@ -11,24 +13,47 @@ var (
 	port int
 )
 
+var (
+	state ServerState
+)
+
+func RecordHandler(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" || r.Method == "" {
+		json.NewEncoder(rw).Encode(state.rawlog)
+	} else {
+		log.Printf("/record was called using %s", r.Method)
+	}
+}
+
+func RunServe(cmd *cobra.Command, args []string) {
+	state = NewServerState()
+
+	// getting the record by GET
+	http.HandleFunc("/data/json", RecordHandler)
+
+	fmt.Printf("Listening on http://localhost:%d/record\n", port)
+	if err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), nil); err != nil {
+		panic(err)
+	}
+}
+
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Runs logging web api on localhost",
-	Run: func(cmd *cobra.Command, args []string) {
-		http.HandleFunc("/record", func(rw http.ResponseWriter, r *http.Request) {
-			fmt.Printf("You requested: %s\n", r.URL.Path)
-		})
-
-		fmt.Printf("Listening on http://localhost:%d/record\n", port)
-		if err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), nil); err != nil {
-			panic(err)
-		}
-	},
+	Run:   RunServe,
 }
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
 
 	serveCmd.Flags().IntVarP(&port, "port", "p", 8000, "--port <PORT NUMBER>")
+}
+
+type ServerState struct {
+	rawlog []string
+}
+
+func NewServerState() ServerState {
+	return ServerState{[]string{"hi"}}
 }
--- vault/Web API.md
+++ vault/Web API.md
@@ -1,27 +1,29 @@
 # Web API for logserve
 
-The api will accept PUT requests to `/log` with logging information to log data and GET requests from `/record`.
+The api will accept PUT requests to `/log` with logging information to log data and GET requests from `/data`.
 
 ## Requests to
 
 Requests are sent to `/log` with PUT in plain text.
 
 - [ ] Logging
-    - `<timestamp> LOG <log information>`
-        Logs the log information
+  - `<timestamp> LOG <log information>`
+    Logs the log information
 - [ ] Key-value
-    - `<timestamp> KEY <key> VALUE`
-        Records the new value for the key
-    - `<timestamp> KEY <key> REMOVE`
-        Removes the key
+  - `<timestamp> KEY <key> VALUE`
+    Records the new value for the key
+  - `<timestamp> KEY <key> REMOVE`
+    Removes the key
 - [ ] Benchmark
-    - `<timestamp> BENCH <name> <start> <finish>`
-        Benchmarks the event
+  - `<timestamp> BENCH <name> <start> <finish>`
+    Benchmarks the event
 
 ## Getting data from the website
 
-Requests are sent to `/record` with GET.
+Requests are sent to `/data` with GET.
 
+- [ ] Static page returning from `/data`
+- [ ] Returning log info in json form from `/data/json`
 - [ ] Display log information
 - [ ] Display Key-value info
 - [ ] Ability to hide log info or key-value info or both
